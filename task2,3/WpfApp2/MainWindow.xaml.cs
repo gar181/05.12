dd4a87644558727c215feb109d1dae960fcc8758
Abushkevich Denis <denabush05@gmail.com>
2023-12-05 16:06:17 +0500

    seconf,third task

---
+++ task2,3/WpfApp2/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Win32;
+
+namespace WpfApp2
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog open = new OpenFileDialog();
+            open.Filter = "(*.txt) | *.txt | (All files)|*.*";
+            string filepath = open.FileName;
+            if(open.ShowDialog() == true)
+            {
+                text.Text = File.ReadAllText(open.FileName);
+            }
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog open = new OpenFileDialog();
+            open.Filter = "(img)| *.png";
+            string filepath = open.FileName;
+            if (open.ShowDialog() == true)
+            {
+                image.Source = new BitmapImage(new Uri(filepath));
+            }
+        }
+    }
+}
